cc7111ad0ca9307e94447b5324a46abee72d87c1
dotakg2 <vagif7007@gmail.com>
2020-10-30 20:04:54 +0600

    first

---
+++ task.py
@@ -0,0 +1,37 @@
+
+a = 12
+b = 15
+c = 3
+d = (a+b*c)
+print(d)
+
+# 12+15*3=57
+
+
+
+a = 5 
+b = 10
+c = b%3
+print(c)
+
+#Po moduly delenie,tolko vozvrashaetca ostatok. ostatok =1
+
+
+
+
+
+age = input("You are age:")
+print("You already" + age + "years")
+
+
+what = input("cto delaem? (+,-):")
+a = int( input("VVedite pervoe chislo:") )
+b = int( input("VVedite vtoroe chislp:") )
+
+if what == "+":
+    c = (a + b)
+    print(c)
+
+
+
+    
---
+++ task1.py
@@ -0,0 +1,33 @@
+# name =  "Hello World"
+# name_2 = "I'm Vagif"
+# res = name + " " + name_2
+# print(res)
+
+
+
+
+# num = "Hello"
+# print(num *3)
+
+
+
+
+
+
+
+# name = "Hello,"
+# name_2 = input("enter name:")
+# name_3 = input("enter first name:")
+# res = name + name_2 + name_3
+# print(res) 
+
+
+
+
+ 
+
+
+
+# a = "hello"
+# b = a[::-1]
+# print(b)                   
